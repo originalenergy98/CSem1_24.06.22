9f885b26357905d65fd43ad911854f1a445d0777
Aleksandra <igoshina.sashenka98@gmail.com>
2022-07-01 16:15:40 +0700

    ДЗ2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿// 1. Напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя) 
+﻿//                                        Семинар 1
+
+// 1. Напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя) 
 
 // Console.Write("Input number:"); // запрашиваем у пользователя число 
 // int number = Convert.ToInt32(Console.ReadLine()); // создаем переменную number и сохраняем в нее значение, введенное из консоли
@@ -47,7 +49,9 @@
 // else 
 //     Console.WriteLine("Не верно указан день");
 
-//                           Домашнее задание
+
+//---------------------------------------------------------------------
+//                           Домашнее задание 1
 
 // Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
@@ -55,15 +59,17 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
-// Console.Write("Input first number: ");
-// int number_1 = Convert.ToInt32(Console.ReadLine());
-// Console.Write("Input second number: ");
-// int number_2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Number a: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Number b: ");
+// int b = Convert.ToInt32(Console.ReadLine());
 
-// if(number_1 > number_2)
-//     Console.Write("a = " + number_1 + ", b = " + number_2 + " -> " + "max = " + number_1 + ", min = " + number_2);
-// else
-//     Console.Write("a = " + number_1 + ", b = " + number_2 + " -> " + "max = " + number_2 + ", min = " + number_1);
+// if(a > b){  //либо это
+//     Console.Write("max = " + a );}
+// else if (a < b){ // либо это
+//     Console.Write("max = " + b );}
+// else { // иначе
+//     Console.Write("введены одинаковые числа");}
 
 // Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
@@ -72,20 +78,20 @@
 // 22 3 9 -> 22
 
 // Console.Write("Input number_1: ");
-// int number_1 = Convert.ToInt32(Console.ReadLine());
+// int num_1 = Convert.ToInt32(Console.ReadLine());
 // Console.Write("Input number_2: ");
-// int number_2 = Convert.ToInt32(Console.ReadLine());
+// int num_2 = Convert.ToInt32(Console.ReadLine());
 // Console.Write("Input number_3: ");
-// int number_3 = Convert.ToInt32(Console.ReadLine());
-// int max = 0; 
+// int num_3 = Convert.ToInt32(Console.ReadLine()); 
 
-// if(number_1 > number_2){
-//     max = number_1;}
-// if(number_2 > number_3){
-//     max = number_2;}
-// else
-//     {max = number_3;}
-// Console.WriteLine("a = " + number_1 + ", b = " + number_2 + ", c = " + number_3 + " -> " + "max = " + max);
+// if(num_1 > num_2 & num_1 > num_3){ //if & - если и то и то верно
+//     Console.WriteLine("max number: " + num_1);}
+// else if(num_2 > num_1 & num_2 > num_3){
+//     Console.WriteLine("max number: " + num_2);}
+// else if(num_3 > num_1 & num_3 > num_2){
+//     Console.WriteLine("max number: " + num_3);}
+// else 
+//     {Console.WriteLine("False");}
 
 // Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 
@@ -96,7 +102,7 @@
 // Console.Write("Input number: ");
 // int number = Convert.ToInt32(Console.ReadLine());
 
-// if(0 == number % 2)
+// if(0 == number % 2) // % - процент от деления
 //     Console.Write(number + "->" + "yes");
 // else
 //     Console.Write(number + "->" + "no");
@@ -109,9 +115,88 @@
 
 // Console.Write("Input number: ");
 // int number = Convert.ToInt32(Console.ReadLine());
+
 // Console.Write(number + " -> ");
 // for( int i = 1; i <= number; i++)
 //     if(0 == i % 2)
 //     Console.Write(" " + i);
+
+//--------------------------------------------------------------------------------------------------------
+//                                     Семинар 2
+// 9. Напишите программу, которачя выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+// 78 -> 8
+// 12 -> 2
+// 85 -> 8
+
+// void randomNumber(){ // метод
+//     int num = new Random().Next(10,100); // последнюю цифру Random не включает
+//         Console.Write(num + " -> ");
+//     if ( num / 10 > num % 10){
+//         Console.WriteLine(num / 10);}
+//     else{Console.WriteLine(num % 10);}
+
+// }
+
+// for (int i = 0; i < 2; i++)
+// {
+//     randomNumber();
+// }
+
+
+// void randomNumber();
+
+// int SumTwoNumbers(int a, int b){ // int возвращает   
+//     int sum = a +b;
+//     return sum;
+// }
+
+// Console.Write("Введите первое число: ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Сумма двух чисел: " + SumTwoNumbers(number1, number2));
+
+//--------------------------------------------------------------------------------------------------------
+//                                Домашнее заадание 2
+
+
+// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+// Console.Write("Введите трехзначное число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int num1 = num / 10;
+// int num2 = num1 % 10;
+// Console.WriteLine(num2);
+
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+Console.Write("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(0 == num / 100){
+    Console.WriteLine("Третьей цифры нет");
+}
+else{while (num > 1000)
+{
+    num = num / 10;
+} 
+    Console.WriteLine(num % 10);}
+
+
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
      
-    
+// Console.Write("Input number: ");
+// int number = Convert.ToInt32(Console.ReadLine());    
